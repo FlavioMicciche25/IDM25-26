81abcd5021c85e70015af1a5413a8dfc753497ad
Flavio Micciché <miccicheflavio25@gmail.com>
2026-09-14 10:41:35 +0200

    fix: correct syntax errors on preprocessor

--- first_classwork/src/preprocessing.py
+++ first_classwork/src/preprocessing.py
@@ -7,9 +7,10 @@ class Preprocessor:
         self.dataset = data.copy()
 
     def dropcol(self):
-        toDrop = ["db_id"], "puntovendita_id","cassa","cassiere","nummero_scontrino",
+        toDrop = ["db_id", "puntovendita_id","cassa","cassiere","nummero_scontrino",
             "num_riga","r_peso", "r_iva", "r_sconto",
             "tipologia", "descr_tipologia", "cod_rep", "descr_rep", "cat_mer"]
 
-            self.dataset = self.dataset.drop(columns=toDrop)
-            return self
+        self.dataset = self.dataset.drop(columns=toDrop)
+        return self
+    
