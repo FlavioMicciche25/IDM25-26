151d64c80da279098677a506262927458178634d
Flavio Micciché <miccicheflavio25@gmail.com>
2026-09-14 10:16:26 +0200

    feat: add preprocessor class

---
+++ first_classwork/src/preprocessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datetime import time
+
+
+class Preprocessor:
+    def __init__(self,data):
+        self.dataset = data.copy()
+
+    def dropcol(self):
+        toDrop = ["db_id"], "puntovendita_id","cassa","cassiere","nummero_scontrino",
+            "num_riga","r_peso", "r_iva", "r_sconto",
+            "tipologia", "descr_tipologia", "cod_rep", "descr_rep", "cat_mer"]
+
+            self.dataset = self.dataset.drop(columns=toDrop)
+            return self
