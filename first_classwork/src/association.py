c1557b54e5045ecca6d807965d4aff669ccfb5ab
fladmin <fhimicciche@gmail.com>
2026-09-17 20:28:12 +0200

    feat: correct syntax error from apriori and add FPGrowth for association

--- first_classwork/src/association.py
+++ first_classwork/src/association.py
@@ -4,7 +4,7 @@ from mlxtend.frequent_patterns import apriori, association_rules, fpgrowth
 
 class Association:
 
-    def __init__(self,data):
+    def __init__(self, data):
         self.dataset : pd.DataFrame = data.copy()
         self.transactions = None
         self.rules = None
@@ -15,12 +15,30 @@ class Association:
         self.transactions = t.tolist()
         return self
 
-    def apriorirules(self,threshold = 0.05, minconfidence = 0.3):
+    def apriorirules(self, threshold = 0.05, minconfidence = 0.3):
         transenc = TransactionEncoder()
         transencAry = transenc.fit(self.transactions).transform(self.transactions)
-        data = pd.DataFrame(transencAry,column=transenc.columns_)
+        data = pd.DataFrame(transencAry,columns=transenc.columns_)
 
         freqitemsets = apriori (data,min_support=threshold,use_colnames=True)
 
         self.rules = association_rules(freqitemsets,metric='confidence',min_threshold=minconfidence)
         return self
+
+    def fpgrowthrules(self, threshold = 0.05, minconfidence = 0.3):
+        
+        transec = TransactionEncoder()
+        transecAry = transec.fit(self.transactions).transform(self.transactions)
+        data = pd.DataFrame(transecAry, columns=transec.columns_)
+
+        
+        freqitemsets = fpgrowth(data, min_support=threshold, use_colnames=True)
+
+
+        self.rules = association_rules(freqitemsets, metric='confidence', min_threshold=minconfidence)
+        return self
+
+    def showrules(self, n = 10):
+        df = self.rules.copy()
+        print(df[['antecedents', 'consequents', 'support', 'confidence', 'lift']].head(n))
+    
