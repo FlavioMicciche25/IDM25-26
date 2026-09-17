b3949c5effa152c3efed70b9c68309d508f56551
fladmin <fhimicciche@gmail.com>
2026-09-17 20:30:27 +0200

    feat: add association utility on main

--- first_classwork/src/main.py
+++ first_classwork/src/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from dataloader import DataLoader
 from preprocessing import Preprocessor
 from frequencyAnalisys import FrequencyAnalyzer
+from association import Association
 
 link = "./AnonymizedFidelity.csv"
 loader = DataLoader(link)
@@ -23,3 +24,11 @@ data : pd.DataFrame = preprocessor.removeshoppers().dropcol().convertdatetime().
 frequencyanalyzer = FrequencyAnalyzer(data)
 frequencyanalyzer.analyzelevels()
 frequencyanalyzer.stratifiedlevels()
+
+
+associator = Association(data)
+print ("APRIORI treshold = 0.05 confidence = 0.2 maxnumrules = 20")
+apriori = associator.create_transaction().apriorirules(0.05,0.2).showrules(20)
+
+print ("FP-GROWTH treshold = 0.05 confidence = 0.2 maxnumrules = 20")
+apriori = associator.create_transaction().fpgrowthrules(0.05,0.15).showrules(30)
